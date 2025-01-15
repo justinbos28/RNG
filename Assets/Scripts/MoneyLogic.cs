7e0dbcb47a9320d2ed250a92fb52eda2f6392c65
Justin Bos <justin_bos@home.nl>
2025-01-15 14:18:07 +0100

    Update shop and images

--- Assets/Scripts/MoneyLogic.cs
+++ Assets/Scripts/MoneyLogic.cs
@@ -7,90 +7,186 @@ using UnityEngine.UI;
 
 public class MoneyLogic : MonoBehaviour
 {
-    public bool BoughtRollSpeed1;
-    public bool BoughtRollSpeed2;
-    public bool BoughtRollSpeed3;
-    public bool BoughtRollSpeed4;
-    public bool BoughtRollSpeed5;
-    public bool BoughtRollSpeed6;
-    public RNGscript RNGscript;
+    public int BoughtRollSpeed;
+    public int BoughtRollAmount;
+    public int BoughtRollSkips;
+    public bool AutoRoll;
+
     public Text RollSpeedText;
     public Text CurrentMoney;
+    public Text RollAmountText;
+    public Text RollSkipsText;
+    public Text AutoRollText;
+    public RNGscript RNGscript;
+    public GameObject AutoRollButton;
     public void BuyRollspeed()
     {
-        if (StaticVariables.cash >= 25 && BoughtRollSpeed1 == false)
+        if (StaticVariables.cash >= 25 && BoughtRollSpeed == 0)
         {
             StaticVariables.cash -= 25;
             RNGscript.RollSpeed = 0.45f;
-            BoughtRollSpeed1 = true;
-            RollSpeedText.text = "250$";
+            BoughtRollSpeed = 1;
+            RollSpeedText.text = "150$";
             CurrentMoney.text = StaticVariables.cash + "$";
         }
-        else if (StaticVariables.cash >= 250 && BoughtRollSpeed2 == false)
+        else if (StaticVariables.cash >= 150 && BoughtRollSpeed == 1)
         {
-            StaticVariables.cash -= 250;
+            StaticVariables.cash -= 150;
             RNGscript.RollSpeed = 0.4f;
-            BoughtRollSpeed2 = true;
-            RollSpeedText.text = "1000$";
+            BoughtRollSpeed = 2;
+            RollSpeedText.text = "500$";
             CurrentMoney.text = StaticVariables.cash + "$";
         }
-        else if (StaticVariables.cash >= 1000 && BoughtRollSpeed3 == false)
+        else if (StaticVariables.cash >= 500 && BoughtRollSpeed == 2)
         {
-            StaticVariables.cash -= 1000;
+            StaticVariables.cash -= 500;
             RNGscript.RollSpeed = 0.35f;
-            BoughtRollSpeed3 = true;
-            RollSpeedText.text = "2500$";
+            BoughtRollSpeed = 3;
+            RollSpeedText.text = "1700$";
             CurrentMoney.text = StaticVariables.cash + "$";
         }
-        else if (StaticVariables.cash >= 2500 && BoughtRollSpeed4 == false)
+        else if (StaticVariables.cash >= 1700 && BoughtRollSpeed == 3)
         {
-            StaticVariables.cash -= 2500;
+            StaticVariables.cash -= 1700;
             RNGscript.RollSpeed = 0.3f;
-            BoughtRollSpeed4 = true;
-            RollSpeedText.text = "7500$";
+            BoughtRollSpeed = 4;
+            RollSpeedText.text = "4500$";
             CurrentMoney.text = StaticVariables.cash + "$";
         }
-        else if (StaticVariables.cash >= 7500 && BoughtRollSpeed5 == false)
+        else if (StaticVariables.cash >= 4500 && BoughtRollSpeed == 4)
         {
-            StaticVariables.cash -= 7500;
+            StaticVariables.cash -= 4500;
             RNGscript.RollSpeed = 0.25f;
-            BoughtRollSpeed5 = true;
-            RollSpeedText.text = "20.000";
+            BoughtRollSpeed = 5;
+            RollSpeedText.text = "10.000";
             CurrentMoney.text = StaticVariables.cash + "$";
         }
-        else if (StaticVariables.cash >= 20000 && BoughtRollSpeed6 == false)
+        else if (StaticVariables.cash >= 10000 && BoughtRollSpeed == 5)
         {
-            StaticVariables.cash -= 20000;
+            StaticVariables.cash -= 10000;
             RNGscript.RollSpeed = 0.2f;
-            BoughtRollSpeed6 = true;
+            BoughtRollSpeed = 6;
             RollSpeedText.text = "Purchased";
             CurrentMoney.text = StaticVariables.cash + "$";
         }
+        //else if (StaticVariables.cash >= 25000 && BoughtRollSpeed == 6)
+        //{
+        //    StaticVariables.cash -= 25000;
+        //    RNGscript.RollSpeed = 0.15f;
+        //    BoughtRollSpeed = 7;
+        //    RollSpeedText.text = "50.000$";
+        //    CurrentMoney.text = StaticVariables.cash + "$";
+        //}
+        //else if (StaticVariables.cash >= 50000 && BoughtRollSpeed == 7)
+        //{
+        //    StaticVariables.cash -= 50000;
+        //    RNGscript.RollSpeed = 0.1f;
+        //    BoughtRollSpeed = 8;
+        //    RollSpeedText.text = "Purchased";
+        //    CurrentMoney.text = StaticVariables.cash + "$";
+        //}
     }
 
     public void BuyAutoRoll()
     {
-        if (StaticVariables.cash >= 5)
+        if (StaticVariables.cash >= 1000 && AutoRoll == false)
         {
-            StaticVariables.cash -= 5;
+            StaticVariables.cash -= 1000;
             RNGscript.AutoTimer = 0;
+            AutoRoll = true;
+            AutoRollButton.SetActive(true);
+            RNGscript.RollButton.SetActive(false);
+            AutoRollText.text = "Purchased";
+        }
+    }
+    public void AutoRollToggle()
+    {
+        if (AutoRoll == false)
+        {
+            RNGscript.AutoTimer = 0;
+            RNGscript.RollButton.SetActive(false);
+            AutoRoll = true;
+        }
+        else if (AutoRoll ==  true)
+        {
+            RNGscript.AutoTimer = 1;
+            RNGscript.RollButton.SetActive(true);
+            AutoRoll = false;
         }
     }
     public void BuyRollAmount()
     {
-        if (StaticVariables.cash >= 500)
+        if (StaticVariables.cash >= 1500 && BoughtRollAmount == 0)
         {
-            StaticVariables.cash -= 500;
-            RNGscript.cardLimit += 1;
+            StaticVariables.cash -= 1500;
+            RNGscript.cardLimit = 2;
+            BoughtRollAmount = 1;
+            RollAmountText.text = "5000$";
+            CurrentMoney.text = StaticVariables.cash + "$";
+        }
+        else if (StaticVariables.cash >= 5000 && BoughtRollAmount == 1)
+        {
+            StaticVariables.cash -= 5000;
+            RNGscript.cardLimit = 3;
+            BoughtRollAmount = 2;
+            RollAmountText.text = "15.000$";
+            CurrentMoney.text = StaticVariables.cash + "$";
+        }
+        else if (StaticVariables.cash >= 15000 && BoughtRollAmount == 2) 
+        {
+            StaticVariables.cash -= 15000;
+            RNGscript.cardLimit = 4;
+            BoughtRollAmount = 3;
+            RollAmountText.text = "45.000$";
+            CurrentMoney.text = StaticVariables.cash + "$";
+        }
+        else if (StaticVariables.cash >= 45000 && BoughtRollAmount == 3)
+        {
+            StaticVariables.cash -= 45000;
+            RNGscript.cardLimit = 5;
+            BoughtRollAmount = 4;
+            RollAmountText.text = "100.000$";
+            CurrentMoney.text = StaticVariables.cash + "$";
+        }
+        else if (StaticVariables.cash >= 100000 && BoughtRollAmount == 4)
+        {
+            StaticVariables.cash -= 100000;
+            RNGscript.cardLimit = 6;
+            BoughtRollAmount = 5;
+            RollAmountText.text = "Purchased";
+            CurrentMoney.text = StaticVariables.cash + "$";
         }
     }
-    public void RollAmountTest()
+    public void BuyRollSkips()
     {
-        RNGscript.cardLimit += 1;
+        if (StaticVariables.cash >= 5000 && BoughtRollSkips == 0)
+        {
+            StaticVariables.cash -= 5000;
+            RNGscript.RollSkips = 3;
+            BoughtRollSkips = 1;
+            RollSkipsText.text = "10.000$";
+            CurrentMoney.text = StaticVariables.cash + "$";
+        }
+        else if(StaticVariables.cash >= 10000 && BoughtRollSkips == 1)
+        {
+            StaticVariables.cash -= 10000;
+            RNGscript.RollSkips = 2;
+            BoughtRollSkips = 2;
+            RollSkipsText.text = "20.000$";
+            CurrentMoney.text = StaticVariables.cash + "$";
+        }
+        else if( StaticVariables.cash >= 20000 && BoughtRollSkips == 2)
+        {
+            StaticVariables.cash -= 20000;
+            RNGscript.RollSkips = 1;
+            BoughtRollSkips = 3;
+            RollSkipsText.text = "Purchased";
+            CurrentMoney.text = StaticVariables.cash + "$";
+        }
     }
     public void GiveMoneyTest()
     {
-        StaticVariables.cash += 1000;
+        StaticVariables.cash += 100000;
         CurrentMoney.text = StaticVariables.cash + "$";
     }
 }
--- Assets/Scripts/RNG script.cs
+++ Assets/Scripts/RNG script.cs
@@ -15,19 +15,27 @@ public class RNGscript : MonoBehaviour
     public List<OreClass> level6Ore = new List<OreClass>();
     public List<OreClass> level7Ore = new List<OreClass>();
     public List<OreClass> level8Ore = new List<OreClass>();
+    public List<OreClass> level9Ore = new List<OreClass>();
+    public List<OreClass> level10Ore = new List<OreClass>();
+    public List<OreClass> level11Ore = new List<OreClass>();
+    public List<OreClass> level12Ore = new List<OreClass>();
     public List<OreClass> playerHand = new List<OreClass>();
     public int cardLimit = 1;
 
     public float timer = 0;
+    public float timer2 = 0;
     public float RollSpeed = 0.5f;
     public int StartTimer;
     public int RollStatus;
     public int RollSkips = 4;
-    public int AutoTimer = 1;  
+    public int AutoTimer = 1;
+    public bool Timer2Active = false;
+    public bool AutoRollTimer = false;
 
-    public Button RollButton;
     public Text RollingText;
     public Text CurrentMoney;
+    public MoneyLogic MoneyLogic;
+    public GameObject RollButton;
     public GameObject RollButton1;
     public GameObject RollButton2;
     public GameObject RollButton3;
@@ -78,7 +86,7 @@ public class RNGscript : MonoBehaviour
         }
     }
 
-    // Assign the cards to list by level
+    // assign the ored to list by level
     public void AssignRarity()
     {
         foreach (OreClass card in allOres)
@@ -131,24 +139,96 @@ public class RNGscript : MonoBehaviour
                 card.rarityEffectColor = Color.black;
                 level8Ore.Add(card);
             }
+            else if (card.rarity == 9)
+            {
+                card.rarityTitle = "Unreal";
+                card.rarityEffectColor = new Vector4(1f, 0.5f, 0.1f ,0f);
+                level9Ore.Add(card);
+            }
+            else if (card.rarity == 10)
+            {
+                card.rarityTitle = "Ancient";
+                card.rarityEffectColor = new Vector4(0.6f, 0.45f, 0.08f, 0f);
+                level10Ore.Add(card);
+            }
+            else if (card.rarity == 11)
+            {
+                card.rarityTitle = "IDon'tKnowAnymore";
+                card.rarityEffectColor = new Vector4(0.8f, 0, 1, 0);
+                level11Ore.Add(card);
+            }
+            else if (card.rarity == 12)
+            {
+                card.rarityTitle = "YouJustGotLucky";
+                card.rarityEffectColor = new Vector4(0, 1, 1, 0);
+                level12Ore.Add(card);
+            }
         }
     }
 
-    // Roll for a new hand
+    // roll for a new hand
     public void RollForHand()
     {
         playerHand.Clear();
         List<int> hand = new List<int>();
         for (int i = 0; i < cardLimit; i++)
         {
-            bool cardRarityChance8 = CalulateRNGPercent(0.01f);
-            bool cardRarityChance7 = CalulateRNGPercent(0.1f);
+            bool cardRarityChance12 = CalulateRNGPercent(0.1f);
+            bool cardRarityChance11 = CalulateRNGPercent(0.13f);
+            bool cardRarityChance10 = CalulateRNGPercent(0.25f);
+            bool cardRarityChance9 = CalulateRNGPercent(0.4f);
+            bool cardRarityChance8 = CalulateRNGPercent(0.625f);
+            bool cardRarityChance7 = CalulateRNGPercent(0.8f);
             bool cardRarityChance6 = CalulateRNGPercent(1);
             bool cardRarityChance5 = CalulateRNGPercent(10);
             bool cardRarityChance4 = CalulateRNGPercent(17);
             bool cardRarityChance3 = CalulateRNGPercent(25);
             bool cardRarityChance2 = CalulateRNGPercent(50);
-            if (cardRarityChance8)
+            if (cardRarityChance12)
+            {
+                if (level12Ore.Count == 1)
+                {
+                    hand.Add(level12Ore[0].OreID);
+                }
+                else
+                {
+                    hand.Add(level12Ore[Random.Range(1, level12Ore.Count)].OreID);
+                }
+            }
+            else if (cardRarityChance11)
+            {
+                if (level11Ore.Count == 1)
+                {
+                    hand.Add(level11Ore[0].OreID);
+                }
+                else
+                {
+                    hand.Add(level11Ore[Random.Range(1, level11Ore.Count)].OreID);
+                }
+            }
+            else if (cardRarityChance10)
+            {
+                if (level10Ore.Count == 1)
+                {
+                    hand.Add(level10Ore[0].OreID);
+                }
+                else
+                {
+                    hand.Add(level10Ore[Random.Range(1, level10Ore.Count)].OreID);
+                }
+            }
+            else if (cardRarityChance9)
+            {
+                if (level9Ore.Count == 1)
+                {
+                    hand.Add(level9Ore[0].OreID);
+                }
+                else
+                {
+                    hand.Add(level9Ore[Random.Range(1, level9Ore.Count)].OreID);
+                }
+            }
+            else if (cardRarityChance8)
             {
                 if (level8Ore.Count == 1)
                 {
@@ -253,7 +333,7 @@ public class RNGscript : MonoBehaviour
         DisplayCard();
     }
 
-    // Displays all the cards
+    // displays all the ores
     public void DisplayCard()
     {
         for (int i = 0; i < playerHand.Count; i++)
@@ -277,6 +357,7 @@ public class RNGscript : MonoBehaviour
 
     public void Update()
     {
+        // timers for auto rolling, animation
         if (StartTimer == AutoTimer)
         {
             timer = timer + Time.deltaTime;
@@ -285,12 +366,39 @@ public class RNGscript : MonoBehaviour
         {
             timer = 0;
         }
+
+        if (AutoRollTimer == true)
+        {
+            timer2 = timer2 + Time.deltaTime;
+        }
+        // delay after getting a ore
+        if (RollStatus >= RollSkips)
+        {
+            if (timer2 > 1) // bigger
+            {
+                if (MoneyLogic.AutoRoll == true)
+                {
+                    RollButton.SetActive(false);
+                }
+                else
+                {
+                    RollButton.SetActive(true);
+                }
+                RollStatus = 0;
+                AutoRollTimer = false;
+                timer2 = 0;
+            }
+            else if (timer2 < 1) // smaller
+            {
+                StartTimer = 0;
+                timer = 0;
+                AutoRollTimer = true;
+            }
+
+        }
+        // showing the ore and giving the price
         if (RollStatus == RollSkips)
         {
-            StartTimer = 0;
-            timer = 0;
-            RollStatus = 0;
-            RollButton.enabled = true;
             RollingText.text = "You rolled ";
             RarityEffectSprite.color = new Color(RarityEffectSprite.color.r, RarityEffectSprite.color.g, RarityEffectSprite.color.b, 0.5f);
             RarityEffectSprite2.color = new Color(RarityEffectSprite.color.r, RarityEffectSprite.color.g, RarityEffectSprite.color.b, 0.3f);
@@ -311,12 +419,14 @@ public class RNGscript : MonoBehaviour
                 if (playerHand[i].OreID >= 6)
                 {
                     AutoTimer = 1;
+                    RollButton.SetActive(true);
+                    MoneyLogic.AutoRoll = false;
                 }
             }
             CurrentMoney.text = StaticVariables.cash + "$";
-            
+            RollStatus++;
         }
-
+        // rolling the dice (roll animation)
         if (timer > RollSpeed)
         {
             timer = 0;
@@ -338,6 +448,7 @@ public class RNGscript : MonoBehaviour
             RollingText.text = "Rolling...";
         }
 
+        // aligns the ores to the corerct spot after buying more roll amount
         if (cardLimit == 6)
         {
             RollButton6.SetActive(true);
@@ -400,13 +511,13 @@ public class RNGscript : MonoBehaviour
             RarityEffectObject2.transform.position = RollButton2.transform.position;
         }
     }
-
+    // button connected first roll
     public void RollDice()
     {
         timer = 0;
         StartTimer = 1;
         RollForHand();
-        RollButton.enabled = false;
+        RollButton.SetActive(false);
         RollingText.enabled = true;
         RarityEffectSprite.color = new Color(RarityEffectSprite.color.r, RarityEffectSprite.color.g, RarityEffectSprite.color.b, 0);
         RarityEffectSprite2.color = new Color(RarityEffectSprite.color.r, RarityEffectSprite.color.g, RarityEffectSprite.color.b, 0);
